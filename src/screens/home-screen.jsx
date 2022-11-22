77814ee5c99d7c3c566f89f1c98cf0dca017dae0
Gerard Siles <gerardsiles@gmail.com>
2022-11-22 00:01:40 +0000

    pie chart implemented

--- src/screens/home-screen.jsx
+++ src/screens/home-screen.jsx
@@ -2,11 +2,6 @@ import React, { useState } from 'react';
 import Papa from 'papaparse';
 import csvFile from '../assets/Artikel.csv';
 import Table from '../components/table-component';
-import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
-import Box from '@mui/material/Box';
-
-// Allowed extensions for input file
-const allowedExtensions = ['csv'];
 
 function Home() {
 	const [data, setData] = useState([]);
@@ -27,13 +22,19 @@ function Home() {
 
 	return (
 		<div className=''>
-			<h3 className='text-3xl font-bold underline text-center'>CSV Parser</h3>
+			<div className='flex-1'>
+				<h3 className='text-3xl font-bold underline text-center'>CSV Parser</h3>
 
-			{/* Parse local CSV */}
-			<button onClick={handleLocalParse} className='m-5'>
-				Parse local CSV
-			</button>
+				{/* Parse local CSV */}
+				<button
+					onClick={handleLocalParse}
+					className='bg-blue-500 hove:bg-blue-700 text-white font-bold py-2 px-4 rounded m-5 '
+				>
+					Parse local CSV
+				</button>
+			</div>
 
+			{/* Render Table if there's data to be shown */}
 			{data.length > 0 && <Table data={data} />}
 
 			{/* Download csv icon https://icons8.com/icons/set/csv-file */}
